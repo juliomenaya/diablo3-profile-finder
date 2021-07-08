dceaecdce535b8602bbd92ddf75a6ea8bf71a70b
juliomenaya <juliomenaya@gmail.com>
2021-07-07 22:27:06 -0500

    lazy loading and keeping alive

--- src/views/Hero/HeroSkills/Index.vue
+++ src/views/Hero/HeroSkills/Index.vue
@@ -3,22 +3,50 @@
     <h2 class="font-diablo">Skills</h2>
     <hr class="bg-white">
 
-    <ActiveSkills :skills="skills.active"/>
-    <hr>
-    <PassiveSkills :skills="skills.passive"/>
+    <b-nav pills small>
+      <b-nav-item :active="!isPassiveSkillsActive" @click="changeComponent('ActiveSkills')">Active</b-nav-item>
+      <b-nav-item :active="isPassiveSkillsActive" @click="changeComponent('PassiveSkills')">Passive</b-nav-item>
+    </b-nav>
+    <!-- Con keep-alive hacemos que los recursos (sprites) del componente a ser renderizado no se carguen
+     siempre que son mostrados en la pantalla. -->
+    <keep-alive>
+      <component :is="activeComponent" :skills="componentProps"/>
+    </keep-alive>
 
   </div>
 </template>
 
 <script>
-import ActiveSkills from './ActiveSkills'
-import PassiveSkills from './PassiveSkills'
-
 export default {
   name: 'HeroSkills',
   components: {
-    ActiveSkills,
-    PassiveSkills
+    ActiveSkills: () => import(/* webpackChunkName: "ActiveSkills" */ './ActiveSkills'),
+    PassiveSkills: () => import(/* webpackChunkName: "PassiveSkills" */ './PassiveSkills')
+  },
+  data () {
+    return {
+      activeComponent: 'ActiveSkills'
+    }
+  },
+  computed: {
+    /**
+   * Dinamyc props for async dynamic components
+   * @returns {String}
+   */
+    // Con esto estamos generando "props" dinámicas
+    // Si el componente es ActiveSkills pasa como props las activas, si no, las pasivas
+    componentProps () {
+      return this.activeComponent === 'ActiveSkills' ? this.skills.active : this.skills.passive
+    },
+    // Nos dice si el componente "HabilidadesPasivas" está activo o no
+    isPassiveSkillsActive () {
+      return this.activeComponent === 'PassiveSkills'
+    }
+  },
+  methods: {
+    changeComponent (component) {
+      this.activeComponent = component
+    }
   },
   props: {
     skills: {
